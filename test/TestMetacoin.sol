404850845234e8f66b0d2c31a093eddc14664844
arisgk <aris.gk@gmail.com>
2017-07-08 15:28:39 +0300

    Added solium linter

--- test/TestMetacoin.sol
+++ test/TestMetacoin.sol
@@ -4,22 +4,22 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/MetaCoin.sol";
 
+
 contract TestMetacoin {
 
-  function testInitialBalanceUsingDeployedContract() {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+    function testInitialBalanceUsingDeployedContract() {
+        MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-    uint expected = 10000;
+        uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-  }
+        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    }
 
-  function testInitialBalanceWithNewMetaCoin() {
-    MetaCoin meta = new MetaCoin();
+    function testInitialBalanceWithNewMetaCoin() {
+        MetaCoin meta = new MetaCoin();
 
-    uint expected = 10000;
+        uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-  }
-
+        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    }
 }
