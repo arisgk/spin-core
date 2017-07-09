0dfaba0a8e1b35f34ec4af1a4cecbdac0021063d
arisgk <aris.gk@gmail.com>
2017-07-09 17:55:47 +0300

    Made initial supply a parameter

--- contracts/token/Gitcoin.sol
+++ contracts/token/Gitcoin.sol
@@ -7,10 +7,9 @@ contract Gitcoin is StandardToken {
     string public name = "Gitcoin";
     string public symbol = "GTC";
     uint256 public decimals = 18;
-    uint256 public INITIAL_SUPPLY = 10000;
 
-    function Gitcoin() {
-        totalSupply = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
+    function Gitcoin(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply;
     }
 }
