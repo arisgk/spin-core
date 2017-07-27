aac6fc003c1f8a926ef3d8db05fca11c95689084
arisgk <aris.gk@gmail.com>
2017-07-27 11:39:04 +0300

    Updated name

--- contracts/token/Spincoin.sol
+++ contracts/token/Spincoin.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.0;
 import "../../node_modules/zeppelin-solidity/contracts/token/StandardToken.sol";
 
 
-contract Gitcoin is StandardToken {
-    string public name = "Gitcoin";
-    string public symbol = "GTC";
+contract Spincoin is StandardToken {
+    string public name = "Spincoin";
+    string public symbol = "SPN";
     uint256 public decimals = 18;
 
-    function Gitcoin(uint256 _initialSupply) {
+    function Spincoin(uint256 _initialSupply) {
         totalSupply = _initialSupply;
         balances[msg.sender] = _initialSupply;
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 /* global artifacts */
-const Gitcoin = artifacts.require('./Gitcoin.sol');
+const Spincoin = artifacts.require('./Spincoin.sol');
 const Repository = artifacts.require('./Repository.sol');
 
 const price = 1;
 
 module.exports = (deployer) => {
-  deployer.deploy(Gitcoin, 10000).then(() =>
-    deployer.deploy(Repository, Gitcoin.address, price) // eslint-disable-line comma-dangle
+  deployer.deploy(Spincoin, 10000).then(() =>
+    deployer.deploy(Repository, Spincoin.address, price) // eslint-disable-line comma-dangle
   );
 };
